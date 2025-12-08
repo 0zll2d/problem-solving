f572b82dc960d1c41176f3f9ebdb40817a801b50
0zll2d <djadpfa1002@gmail.com>
2025-12-08 22:42:17 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -6,9 +6,18 @@ using namespace std;
 int solution(int n) {
     int answer = 0;
     
-    for(int i = 1; i <= n; i++) {
+    // 소수 판별 알고리즘 참고 (12977. 소수 만들기)
+    for(int i = 1; i * i <= n; i++) {
         if(n % i == 0) {
-            answer += i;
+            int j = n / i;
+            
+            // i와 j는 약수의 쌍
+            answer += i + j;
+            
+            // 예외 처리 (예: n = 36)
+            if(i == j) {
+                answer -= i;
+            }
         }
     }
     
