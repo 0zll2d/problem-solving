a6b1c4f6803495fc6a948cd8b164ac5845c05b91
0zll2d <djadpfa1002@gmail.com>
2025-12-14 20:07:01 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.cpp
@@ -4,30 +4,23 @@
 
 using namespace std;
 
+int idx;
+
+bool comp(const string& a, const string &b) {
+    if(a[idx] == b[idx]) {
+        return a < b;
+    }
+    return a[idx] < b[idx];
+}
+
 vector<string> solution(vector<string> strings, int n) {
+    idx = n;
 
-    sort(strings.begin(), strings.end(),
-        // [n] : 캡처 목록
-        // - 람다는 외부 변수를 직접 사용할 수 없으므로 기준값 n을 캡처
-        // - 값 캡처를 사용해 정렬 도중 기준값이 변하지 않도록 보장
-        [n](const auto& a, const auto& b) {
-            // auto :
-            // - 컴파일 시점에 타입을 자동 추론
-            // - 여기서는 strings의 요소 타입(string)으로 추론됨
-            // const auto& :
-            // - 문자열 복사를 방지하기 위해 참조 사용
-            // - 비교만 수행하므로 const로 수정 방지
-            if (a[n] == b[n]) {
-                return a < b;      // n번째 문자가 같으면 사전순
-            }
-            return a[n] < b[n];    // n번째 문자 기준 오름차순
-        }
-    );
-
+    sort(strings.begin(), strings.end(), comp);
+    
     return strings;
 }
 
 // [커스텀 정렬 정리]
-// - [] : 람다 캡처 목록 (외부 변수 n 사용)
-// - 값 캡처 : 정렬 기준의 안정성 보장
-// - const auto& : 복사 비용 제거 + 읽기 전용 비교
+// 일반 Compare 함수 분리
+// 일반 메서드의 매개 변수로 auto 사용 금지: 타입 추론이 불가능하기 때문
