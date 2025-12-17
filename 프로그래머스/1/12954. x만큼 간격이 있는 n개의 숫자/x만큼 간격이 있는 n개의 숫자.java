cc2af67b9e928bde28cef1851fa5d08969554795
0zll2d <djadpfa1002@gmail.com>
2025-12-18 07:51:06 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,12 +1,16 @@
 class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
-        answer[0] = x;
         
-        for(int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] + x;
+        for(int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i + 1);
         }
         
         return answer;
     }
 }
+
+// 왜 long으로 형변환이 필요할까?
+// x와 i + 1 모두 int 형이기 때문에 int형의 결과값이 long으로 변환이 됨
+// x = 10,000,000(천 만),  n = 1000일 때
+// 10,000,000,000(백 억)으로 int의 범위를 초과해 overflow 발생하고, long으로 변환 시 이상한 값이 저장됨
