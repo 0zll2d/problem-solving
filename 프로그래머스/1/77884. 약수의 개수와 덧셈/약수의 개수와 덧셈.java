b3347e28e6863b335552513ba4e3a0597502fc3a
0zll2d <djadpfa1002@gmail.com>
2025-12-30 07:46:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.05 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -1,26 +1,38 @@
 class Solution {
     public int solution(int left, int right) {
         int answer = 0;
-
-        for (int i = left; i <= right; i++) {
-            answer += (countDivisors(i) % 2 == 0) ? i : -i;
-        }
-
-        return answer;
-    }
-
-    private int countDivisors(int num) {
-        int count = 0;
-
-        for (int i = 1; i * i <= num; i++) {
-            if (num % i == 0) {
-                count += 2;
-                if (i * i == num) {
-                    count--; // 제곱수 보정
-                }
+        
+        for(int i = left; i <= right; i++) {
+            // 약수의 개수가 홀수 = 완전 제곱수 (예: 25, 36)
+            int sqrt = (int) Math.sqrt(i);
+            
+            if(sqrt * sqrt == i) {
+                answer -= i;
+            } else {
+                answer += i;
             }
         }
-
-        return count;
+        
+        return answer;
     }
 }
+
+/**
+ * [ i / sqrt == sqrt 가 적절하지 않은 이유 ]
+ *
+ * i / sqrt 는 정수 나눗셈으로 수행되기 때문에
+ * 나눗셈 결과의 소수점 이하가 모두 버려진다.
+ *
+ * 이로 인해 완전제곱수 여부를 정확하게 판단할 수 없다.
+ *
+ * 예시)
+ *  - i = 15
+ *  - sqrt = (int) Math.sqrt(15) = 3
+ *
+ *  정수 나눗셈 결과:
+ *  - i / sqrt = 15 / 3 = 5
+ *
+ * 따라서 완전제곱수 판별은
+ *  - sqrt * sqrt == i
+ * 와 같이 곱셈을 이용한 정수 비교 방식이 안전하고 권장된다.
+ */
