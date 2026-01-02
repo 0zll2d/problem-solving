d367accf0916f59419b1c9151d99ba550be93c40
0zll2d <djadpfa1002@gmail.com>
2026-01-02 12:27:35 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 8.75 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i = 1; i < food.length; i++) {
+            answer += String.valueOf(i).repeat(food[i] / 2);    
+        }
+        
+        answer += "0";
+        
+        for(int i = food.length - 1; i > 0; i--) {
+            answer += String.valueOf(i).repeat(food[i] / 2);
+        }
+        
+        return answer;
+    }
+}
