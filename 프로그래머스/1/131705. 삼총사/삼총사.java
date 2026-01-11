b82dbbc92187ff4595b7d4b90d08d4f480907c05
0zll2d <djadpfa1002@gmail.com>
2026-01-11 18:10:00 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,17 +1,29 @@
 class Solution {
+    private int answer = 0;
+    
     public int solution(int[] number) {
-        int answer = 0;
-        
-        for(int i = 0; i < number.length; i++) {
-            for(int j = i + 1; j < number.length; j++) {
-                for(int k = j + 1; k < number.length; k++) {
-                    if(number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
+        makeTrio(number, 0, 0, 0);
+        return answer;
+    }
+    
+    private void makeTrio(int[] number, int index, int sum, int count) {
+        // 3개를 이미 골랐으면 여기서 판정
+        if (count == 3) {
+            if (sum == 0) {
+                answer++;
             }
+            return;
+        }
+
+        // 끝까지 갔는데 3개 못 고른 경우
+        if (index == number.length) {
+            return;
         }
         
-        return answer;
+        // 선택 O
+        makeTrio(number, index + 1, sum + number[index], count + 1);
+        
+        // 선택 X
+        makeTrio(number, index + 1, sum, count);
     }
 }
