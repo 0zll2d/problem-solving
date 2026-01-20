3ee51b54566246212a7db9d15c99ec9610125958
0zll2d <djadpfa1002@gmail.com>
2026-01-20 12:28:37 +0900

    [level 1] Title: 모의고사, Time: 3.90 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,17 +7,20 @@ class Solution {
         int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
         int[] score = new int[4];
-        int max = 0;
 
-        score[1] = grade(answers, person1);
-        max = Math.max(max, score[1]);
+        for(int i = 0; i < answers.length; i++) {
+            if(answers[i] == person1[i % person1.length]) {
+                score[1]++;
+            }
+            if(answers[i] == person2[i % person2.length]) {
+                score[2]++;
+            }
+            if(answers[i] == person3[i % person3.length]) {
+                score[3]++;
+            }
+        }
 
-        score[2] = grade(answers, person2);
-        max = Math.max(max, score[2]);
-
-        score[3] = grade(answers, person3);
-        max = Math.max(max, score[3]);
-
+        int max = Math.max(score[1], Math.max(score[2], score[3]));
         List<Integer> answer = new ArrayList<>();
 
         for(int i = 1; i <= 3; i++) {
@@ -27,17 +30,5 @@ class Solution {
         }
 
         return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private int grade(int[] answer, int[] person) {
-        int correct = 0;
-
-        for(int i = 0; i < answer.length; i++) {
-            if(answer[i] == person[i % person.length]) {
-                correct++;
-            }
-        }
-
-        return correct;
     }
 }
