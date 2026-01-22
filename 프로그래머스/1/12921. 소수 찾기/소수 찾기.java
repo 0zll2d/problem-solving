0717478bc466e8e0aaf331894cf1bf54ed81c1b7
0zll2d <djadpfa1002@gmail.com>
2026-01-22 12:44:49 +0900

    [level 1] Title: 소수 찾기, Time: 44.61 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,28 +1,31 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        for(int num = 2; num <= n; num++) {
-            // 2이면 소수이므로 추가
-            if(num == 2) {
-                answer++;
-                continue;
-            }
+        // 에라토스테네스의 체: 소수 판별 알고리즘이 필요 없음(배수를 제거하면 자연스럽게 남는 것이 소수이기 때문)
+        // * 시간복잡도: O(n log log n)
+        boolean[] isPrime = new boolean[n + 1];
 
-            boolean isPrime = true;
-
-            // 약수 찾기 알고리즘 기반으로 소수 판별
-            for(int i = 2; i * i <= num; i++) {
-                if(num % i == 0) {
-                    isPrime = false;
-                    break;
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        for(int i = 2; i * i <= n; i++) {
+            if(isPrime[i]) {
+                for(int j = i * 2; j <= n; j += i) {
+                    isPrime[j] = false;
                 }
             }
+        }
 
-            if(isPrime) {
+        for(int i = 1; i <= n; i++) {
+            if(isPrime[i]) {
                 answer++;
             }
         }
+
 
         return answer;
     }
