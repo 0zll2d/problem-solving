8651eef4c2cd6fe509ea2f2a6ec0be549ad46360
0zll2d <djadpfa1002@gmail.com>
2026-02-01 11:57:12 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 5.90 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -2,30 +2,11 @@ import java.util.*;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        Map<String, Integer> m = new HashMap<>();
-        m.put("code", 0);
-        m.put("date", 1);
-        m.put("maximum", 2);
-        m.put("remain", 3);
+        List<String> list = List.of("code", "date", "maximum", "remain");
+        int idx_ext = list.indexOf(ext);
+        int idx_sort_by = list.indexOf(sort_by);
         
-        int idx_ext = m.get(ext);
-        int idx_sort_by = m.get(sort_by);
-        
-        List<int[]> list = new ArrayList<>();
-        
-        for(int[] d : data) {
-            if(d[idx_ext] < val_ext) {
-                list.add(d);
-            }
-        }
-        
-        Collections.sort(list, (d1, d2) -> Integer.compare(d1[idx_sort_by], d2[idx_sort_by]));
-        
-        int[][] answer = new int[list.size()][4];
-        
-        for(int i = 0; i < list.size(); i++) {
-           answer[i] = list.get(i);
-        }
+        int[][] answer = Arrays.stream(data).filter(d -> d[idx_ext] < val_ext).sorted((d1, d2) -> d1[idx_sort_by] - d2[idx_sort_by]).toArray(int[][]::new);
         
         return answer;
     }
