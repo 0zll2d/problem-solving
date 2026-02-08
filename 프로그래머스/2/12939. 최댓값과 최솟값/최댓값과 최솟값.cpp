22564b48579e352c6317ba480d7b08da2901f48a
0zll2d <djadpfa1002@gmail.com>
2026-02-09 07:42:32 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -8,13 +8,13 @@ using namespace std;
 string solution(string s) {
     stringstream ss(s); // 문자열 공백으로 자르기
     vector<int> nums;
-    string snum;
+    int num;
     
-    while(ss >> snum) {
-        nums.push_back(stoi(snum));
+    while(ss >> num) {     // string -> int 바로 받기
+        nums.push_back(num);
     }
     
     sort(nums.begin(), nums.end());
     
-    return to_string(nums[0]) + " " + to_string(nums[nums.size() - 1]);   // 정수 -> 문자열
+    return to_string(nums.front()) + " " + to_string(nums.back());   // 정수 -> 문자열
 }
