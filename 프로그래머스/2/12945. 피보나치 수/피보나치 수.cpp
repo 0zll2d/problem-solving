ade64a3ab29af9ee38b6caef5ba47087eca6fd71
0zll2d <djadpfa1002@gmail.com>
2026-02-10 08:30:22 +0900

    [level 2] Title: 피보나치 수, Time: 0.54 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> F(n + 1);
+    F[0] = 0;
+    F[1] = 1;
+    
+    for(int i = 2; i <= n; i++) {
+        F[i] = (F[i - 2] + F[i - 1]) % 1234567;
+    }
+
+    return F[n];
+}
