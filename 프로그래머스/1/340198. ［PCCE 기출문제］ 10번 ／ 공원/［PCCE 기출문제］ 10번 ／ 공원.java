01d3aa9b4a11167b48471b614f4e7366249336ea
0zll2d <djadpfa1002@gmail.com>
2026-02-25 08:20:18 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 2.23 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int answer = 0;
-
         int h = park.length;
         int w = park[0].length;
 
         Arrays.sort(mats);  // 매트 오름차순 정렬
 
+        int idx = -1;    // 가장 큰 매트의 인덱스
+
         for(int i = 0; i < h; i++) {  // 50
             for(int j = 0; j < w; j++) {   // 50
                 if("-1".equals(park[i][j])) {
-                    for(int mat : mats) {   // 10 -> answer 값에 바로 최댓값 저장하지 않고, 인덱스를 저장한다면, 해당 인덱스부터 검사 가능
-                        if(isOutOfBounds(h, w, mat, i, j)) {    // 경계값 체크
+                    for(int k = idx + 1; k < mats.length; k++) {   // 가장 큰 매트의 인덱스를 저장하고, 해당 인덱스 다음 더 큰 매트부터 반복문 순회하며 검사
+                        if(isOutOfBounds(h, w, mats[k], i, j)) {    // 경계값 체크
                             break;
                         }
-                        if(check(mat, park, i, j)) {    // 50 * 50
-                            answer = Math.max(answer, mat);
+                        if(check(mats[k], park, i, j)) {    // 50 * 50
+                            idx = k;
                         }
                     }
                 }
             }
         }
 
-        return answer == 0 ? -1 : answer;
+        return idx == -1 ? idx : mats[idx];
     }
 
     private boolean check(int mat, String[][] park, int x, int y) {
