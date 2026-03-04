65e49772c63cb23d7e89f086fe7f26e7c9c7a43e
0zll2d <djadpfa1002@gmail.com>
2026-03-05 08:23:51 +0900

    [level 1] Title: 신고 결과 받기, Time: 139.24 ms, Memory: 159 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = new int[id_list.length];
+        int N = id_list.length;
+        int[] answer = new int[N];
 
         // 1. 신고 중복 제거
         Set<String> st = new HashSet<>();
@@ -10,42 +11,27 @@ class Solution {
             st.add(rep);
         }
 
-        // 2. 유저 ID와 인덱스 매핑
+        // 2. 사용자 ID와 인덱스 매핑
+        // 3. 신고 당한 사람 기준으로 신고자 ID 리스트 매핑
         Map<String, Integer> m = new HashMap<>();
-        List<List<String>> report_list = new ArrayList<>();   // 유저 신고한 ID 리스트
-
-        for(int i = 0; i < id_list.length; i++) {
+        List<List<String>> report_list = new ArrayList<>();
+        for(int i = 0; i < N; i++) {
             m.put(id_list[i], i);
             report_list.add(new ArrayList<>());
         }
 
-        // 3. 신고 당한 횟수 세기
-        int[] count = new int[id_list.length];
-
         for(String s : st) {
             String[] ids = s.split(" ");
-            count[m.get(ids[1])]++; // 신고 당한 횟수 세기
-            report_list.get(m.get(ids[0])).add(ids[1]);
+            report_list.get(m.get(ids[1])).add(ids[0]); // 신고 당한 사람 기준으로 신고자 리스트 생성
         }
 
-        // 4. 이용 정지 확인하기
-        boolean[] suspended = new boolean[id_list.length];
-
-        for(int i = 0; i < id_list.length; i++) {
-            if(count[i] >= k) {
-                suspended[i] = true;
-            }
-        }
-
-        // 5. 이용 정지 계정에 대해 신고자에게 메일 발송하기
-        for(int i = 0; i < id_list.length; i++) {
-            int result = 0; // 신고 결과
-            for(int j = 0; j < report_list.get(i).size(); j++) {
-                if(suspended[m.get(report_list.get(i).get(j))]) {
-                    result++;
+        // 4. 이용 정지 확인 및 신고자에게 메일 발송
+        for(int i = 0; i < N; i++) {
+            if(report_list.get(i).size() >= k) {
+                for(int j = 0; j < report_list.get(i).size(); j++) {
+                    answer[m.get(report_list.get(i).get(j))]++;
                 }
             }
-            answer[i] = result;
         }
 
         return answer;
