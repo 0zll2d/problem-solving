e5de9642b3f9b8c33e44fae5565b1c043d55f2b7
0zll2d <djadpfa1002@gmail.com>
2026-03-09 08:39:41 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 12.54 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -13,9 +13,9 @@ class Solution {
             m.put(friends[i], i);
         }
 
-        // 2. 이번 달 주고 받은 선물 구하기
+        // 2. 이번 달 주고 받은 선물 구하면서 동시에 선물지수 구하기(선물 지수 = 준 선물 개수 - 받은 선물의 개수)
         int[][] giveAndTake = new int[N][N];    // 주고 받은 선물
-        int[][] giftIndex = new int[N][3];      // 선물 지수
+        int[] giftIndex = new int[N];      // 선물 지수
 
         for(String gift : gifts) {
             String[] s = gift.split(" ");
@@ -23,16 +23,13 @@ class Solution {
             int take = m.get(s[1]);
 
             giveAndTake[give][take]++;
-            giftIndex[give][0]++;
-            giftIndex[take][1]++;
+            
+            // giftIndex 일차원 배열 기반으로 계산하기
+            giftIndex[give]++;
+            giftIndex[take]--;
         }
-
-        // 3. 주고 받은 선물로 선물지수 계산하기
-        for(int i = 0; i < N; i++) {
-            giftIndex[i][2] = giftIndex[i][0] - giftIndex[i][1];
-        }
-
-        // 4. 선물지수 기반으로 다음 달 받을 선물의 개수 계산하기
+        
+        // 3. 선물지수 기반으로 다음 달 받을 선물의 개수 계산하기
         for(int i = 0; i < N; i++) {
             int next = 0;   // 다음 달에 받을 선물의 개수
 
@@ -41,7 +38,7 @@ class Solution {
                     continue;
                 }
                 if(giveAndTake[i][j] == giveAndTake[j][i]) { // 두 사람이 선물을 주고 받은 기록이 하나도 없거나 주고받은 수가 같다면
-                    if(giftIndex[i][2] > giftIndex[j][2]) {
+                    if(giftIndex[i] > giftIndex[j]) {
                         next++;
                     }
                 } else if(giveAndTake[i][j] > 0 || giveAndTake[j][i] > 0) { // 두 사람이 선물을 주고 받았다면
