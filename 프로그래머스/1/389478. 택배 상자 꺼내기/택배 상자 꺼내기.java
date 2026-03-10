6b624b893c300e93278a07578fe58e2d97a4820e
0zll2d <djadpfa1002@gmail.com>
2026-03-11 08:43:31 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,45 +1,38 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int w, int num) {
 
-        int height = (n + w - 1) / w; // 전체 층 수
+        int h = n % w == 0 ? n / w : n / w + 1; // 전체 층 수
 
-        int[][] boxPosition = new int[n + 1][2]; // 각 박스의 위치 [row, col]
-        List<List<Integer>> columnBoxes = new ArrayList<>(); // 열별 박스 번호 저장
+        int[][] boxes = new int[h][w];
+        int row = 0;
+        int col = 0;
 
-        for (int col = 0; col < w; col++) {
-            columnBoxes.add(new ArrayList<>());
-        }
-
-        int boxNumber = 1;
-
-        for (int row = 0; row < height; row++) {
-
-            if (row % 2 == 0) { // 왼 -> 오른
-                for (int col = 0; col < w && boxNumber <= n; col++) {
-                    boxPosition[boxNumber][0] = row;
-                    boxPosition[boxNumber][1] = col;
-
-                    columnBoxes.get(col).add(boxNumber);
-                    boxNumber++;
+        for(int i = 0, b = 1; i < h; i++) {
+            if(i % 2 == 0) {
+                for(int j = 0; j < w && b <= n; j++) {
+                    if(b == num) {
+                        row = i;
+                        col = j;
+                    }
+                    boxes[i][j] = b++;
                 }
-
-            } else { // 오른 -> 왼
-                for (int col = w - 1; col >= 0 && boxNumber <= n; col--) {
-                    boxPosition[boxNumber][0] = row;
-                    boxPosition[boxNumber][1] = col;
-
-                    columnBoxes.get(col).add(boxNumber);
-                    boxNumber++;
+            } else {
+                for(int j = w - 1; j >= 0 && b <= n; j--) {
+                    if(b == num) {
+                        row = i;
+                        col = j;
+                    }
+                    boxes[i][j] = b++;
                 }
             }
         }
 
-        int targetRow = boxPosition[num][0];
-        int targetCol = boxPosition[num][1];
+        int answer = h - row;
 
-        return columnBoxes.get(targetCol).size() - targetRow;
+        if(boxes[h - 1][col] == 0) {
+            answer--;
+        }
+
+        return answer;
     }
-
 }
