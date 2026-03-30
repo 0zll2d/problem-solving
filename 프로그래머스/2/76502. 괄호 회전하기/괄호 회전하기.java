2b4dde3d6f9b84c157be6abcfddb7160ac77a84b
0zll2d <djadpfa1002@gmail.com>
2026-03-31 08:17:30 +0900

    [level 2] Title: 괄호 회전하기, Time: 45.52 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    Map<Character, Character> bracket;
+
+    boolean check(Deque<Character> q) {
+        Stack<Character> stk = new Stack<>();
+
+        while(!q.isEmpty()) {
+            char c = q.poll();
+
+            if(c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else {
+                if(stk.isEmpty() || bracket.get(c) != stk.peek()) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+
+        return stk.isEmpty();
+    }
+
+    public int solution(String s) {
+        int answer = 0;
+
+        bracket = new HashMap<>();
+        bracket.put(')', '(');
+        bracket.put(']', '[');
+        bracket.put('}', '{');
+
+        Deque<Character> q = new ArrayDeque<>();
+
+        for(char c : s.toCharArray()) {
+            q.offer(c);
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            // 1. 괄호 회전
+            q.offer(q.poll());
+
+            // 2. 올바른 괄호인지 검사
+            if(check(new ArrayDeque<>(q))) {    // check(q)는 값 복사가 아닌 참조값 복사
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
