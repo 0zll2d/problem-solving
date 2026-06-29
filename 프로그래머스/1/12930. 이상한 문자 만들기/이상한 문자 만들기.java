6cffdc197d034d09fa453fd23887f013843cd7f3
0zll2d <djadpfa1002@gmail.com>
2026-06-29 19:30:03 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.09 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,21 +1,21 @@
 class Solution {
     public String solution(String s) {
+        
         StringBuilder sb = new StringBuilder();
         int idx = 0;
         
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == ' ') {
-                sb.append(' ');
+        for(char c : s.toCharArray()) {
+            if(c == ' ') {
                 idx = 0;
-                continue;
-            }
-            
-            if(idx % 2 == 0) {
-                sb.append(Character.toUpperCase(s.charAt(i)));
             } else {
-                sb.append(Character.toLowerCase(s.charAt(i)));
+                if(idx % 2 == 0) {
+                    c = Character.toUpperCase(c);
+                } else {
+                    c = Character.toLowerCase(c);
+                }
+                idx++;
             }
-            idx++;
+            sb.append(c);
         }
         
         return sb.toString();
