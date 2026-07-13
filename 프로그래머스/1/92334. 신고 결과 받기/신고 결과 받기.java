0221148339bfbbc2b3c7715830a78c0d562be232
0zll2d <djadpfa1002@gmail.com>
2026-07-14 07:53:57 +0900

    [level 1] Title: 신고 결과 받기, Time: 124.64 ms, Memory: 206 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -3,37 +3,36 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int N = id_list.length;
-        int[] answer = new int[N];
-
-        // 1. 신고 중복 제거
-        Set<String> st = new HashSet<>();
-        for(String rep: report) {
-            st.add(rep);
-        }
-
-        // 2. 사용자 ID와 인덱스 매핑
-        // 3. 신고 당한 사람 기준으로 신고자 ID 리스트 매핑
-        Map<String, Integer> m = new HashMap<>();
-        List<List<String>> report_list = new ArrayList<>();
+        int[] answer = new int[N];  // 이용자별 처리 결과 메일 받은 횟수
+        
+        Map<String, Integer> info = new HashMap<>();
+        
         for(int i = 0; i < N; i++) {
-            m.put(id_list[i], i);
-            report_list.add(new ArrayList<>());
+            info.put(id_list[i], i);
         }
-
-        for(String s : st) {
-            String[] ids = s.split(" ");
-            report_list.get(m.get(ids[1])).add(ids[0]); // 신고 당한 사람 기준으로 신고자 리스트 생성
+        
+        // 동일한 신고 report는 제거
+        Set<String> unique = new HashSet<>();
+        
+        for(String r : report) {
+            unique.add(r);
         }
-
-        // 4. 이용 정지 확인 및 신고자에게 메일 발송
-        for(int i = 0; i < N; i++) {
-            if(report_list.get(i).size() >= k) {
-                for(int j = 0; j < report_list.get(i).size(); j++) {
-                    answer[m.get(report_list.get(i).get(j))]++;
-                }
+        
+        
+        int[] reported = new int[N];   // 이용자별 신고 당한 횟수
+        
+        for(String u : unique) {
+            String[] ids = u.split(" ");
+            reported[info.get(ids[1])]++;
+        }
+        
+        for(String u : unique) {
+            String[] ids = u.split(" ");
+            if(reported[info.get(ids[1])] >= k) {
+                answer[info.get(ids[0])]++;
             }
         }
-
+        
         return answer;
     }
 }
