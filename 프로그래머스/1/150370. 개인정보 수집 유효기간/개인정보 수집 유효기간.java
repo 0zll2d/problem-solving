a06a7f1cedee6b750951ece9fcd3fffd82120777
0zll2d <djadpfa1002@gmail.com>
2026-07-15 07:57:12 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 2.65 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -3,30 +3,38 @@ import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-
-        Map<String, Integer> m = new HashMap<>();    // 약관 유형별 유효기간(일수)
-
-        for(int i = 0; i < terms.length; i++) {
-            String[] term = terms[i].split(" ");
-            m.put(term[0], Integer.parseInt(term[1]) * 28);
+        
+        // 1. 약관 매핑
+        Map<String, Integer> tm = new HashMap<>();
+        
+        for(String term : terms) {
+            String[] s = term.split(" ");
+            tm.put(s[0], Integer.parseInt(s[1]) * 28);
         }
-
-        int[] t = Arrays.stream(today.split("\\.")).mapToInt(Integer::parseInt).toArray();  // 오늘 날짜 파싱
-        int tday = t[0] * 12 * 28 + (t[1] - 1) * 28 + t[2]; // 달 계산 시 -1 하는 이유: 해당 월이 시작하기 전까지의 개월수 계산이 필요하기 때문
-
+        
+        // 2. 오늘 날짜 일수로 계산
+        int now = calculatedDate(today);
+        
+        // 3. 약관 정보 기준으로 유효기간 검사
         for(int i = 0; i < privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
-            int[] p = Arrays.stream(privacy[0].split("\\.")).mapToInt(Integer::parseInt).toArray();
-            String type = privacy[1];
-
-            int pday = p[0] * 12 * 28 + (p[1] - 1) * 28 + p[2];
-
-            if((tday - pday) >= m.get(type)) {
+            String[] s = privacies[i].split(" ");
+            int date = calculatedDate(s[0]);
+            String term = s[1];
+            int diff = now - date;
+            
+            if(tm.get(term) <= diff) {
                 answer.add(i + 1);
             }
         }
-
+        
         return answer.stream().mapToInt(i -> i).toArray();
     }
+    
+    private int calculatedDate(String date) {
+        int year = Integer.parseInt(date.substring(0, 4));
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
+        
+        return day + month * 28 + year * 12 * 28;
+    }
 }
-
