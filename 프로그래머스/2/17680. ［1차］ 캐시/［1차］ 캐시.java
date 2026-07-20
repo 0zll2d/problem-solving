74035bc1ab2385bd3c6dfc5a408a04b9b14926f9
0zll2d <djadpfa1002@gmail.com>
2026-07-20 12:21:31 +0900

    [level 2] Title: [1차] 캐시, Time: 27.12 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,35 +2,26 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        // 1. 엣지 케이스: 캐시 크기가 0인 경우
-        if(cacheSize == 0) {
-            return cities.length * 5;
-        }
-
         int answer = 0;
-
-        Set<String> st = new HashSet<>();   // 캐시에 저장된 도시
-        Deque<String> dq = new ArrayDeque<>();  // 캐시 교체 알고리즘(LRU)
-
+        
+        List<String> cache = new ArrayList<>();
+        
         for(String city : cities) {
-            city = city.toLowerCase();
-
-            if(st.contains(city)) {     // cache hit
-                dq.remove(city);    // LRU를 위해 삭제 후 추가
-                dq.offer(city);
-                answer += 1;
-            } else {    // cache miss
-                if(dq.size() == cacheSize) {    // 삭제
-                    String rm = dq.peek();
-                    st.remove(rm);
-                    dq.poll();
+            int idx = cache.indexOf(city.toLowerCase());
+            
+            cache.add(city.toLowerCase());  
+            
+            if(idx == -1) {             // cache miss
+                if(cache.size() > cacheSize) {
+                    cache.remove(0);
                 }
-                st.add(city);
-                dq.offer(city);
                 answer += 5;
+            } else {                    // cache hit
+                cache.remove(idx);
+                answer += 1;
             }
         }
-
+        
         return answer;
     }
 }
