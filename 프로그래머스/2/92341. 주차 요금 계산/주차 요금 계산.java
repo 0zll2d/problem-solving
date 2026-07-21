01b47fa5d006a854431e908b9bc61fcc5f942a94
0zll2d <djadpfa1002@gmail.com>
2026-07-21 12:26:14 +0900

    [level 2] Title: 주차 요금 계산, Time: 6.98 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -2,62 +2,55 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-
-        Map<String, String> info = new HashMap<>();   // { 차량 번호, 입차 시간 }
-        Map<String, Integer> res = new TreeMap<>();   // { 차량 번호, 누적 시간 }   +   key 값으로 정렬되는 TreeMap 사용
-
-        // 1. 입/출차 내역에서 차량 별 누적 주차 시간 구하기
+        List<Integer> answer = new ArrayList<>();
+        
+        // 1. 차랑별 누적 시간 계산
+        Map<String, Integer> in = new HashMap<>();
+        Map<String, Integer> parking = new TreeMap<>();  // 차량별 누적 주차 시간(차량 번호순 - TreeMap)
+        
         for(String record : records) {
-            String[] r = record.split(" ");
-
-            // 시각
-            int hour = Integer.parseInt(r[0].substring(0, 2));
-            int min = Integer.parseInt(r[0].substring(3, 5));
-
-            String num = r[1];  // 차량 번호
-            String cmd = r[2];  // 내역
-
-            if("IN".equals(cmd)) {
-                info.put(num, r[0]);
-            } else {    // "OUT" 이면
-                String time = info.get(num);
-                int inHour = Integer.parseInt(time.substring(0, 2));
-                int inMin = Integer.parseInt(time.substring(3, 5));
-
-                int diff = hour * 60 + min - (inHour * 60 + inMin); // 누적 주차 시간 구하기
-
-                info.remove(num);  // 맵에서 제거(또 입차할 가능성)
-                res.put(num, res.getOrDefault(num, 0) + diff);    // 차별 누적 주차 시간 추가
+            String[] s = record.split(" ");
+            String car = s[1];
+            int time = calculateTime(s[0]);
+            
+            if("IN".equals(s[2])) {
+                in.put(car, time);
+            } else {    // "OUT"
+                parking.put(car, parking.getOrDefault(car, 0) + time - in.get(car)); 
+                in.remove(car);
             }
         }
-
-        // 2. 입차된 이후 출차된 내역이 없는 차량의 누적 주차 시간 구하기
-        for(String num : new ArrayList<>(info.keySet())) {  // 안전하게 key 복사해서 사용
-            String time = info.get(num);
-            int inHour = Integer.parseInt(time.substring(0, 2));
-            int inMin = Integer.parseInt(time.substring(3, 5));
-
-            int diff = 23 * 60 + 59 - (inHour * 60 + inMin); // 23:59 출차로 간주하고 계산
-
-            info.remove(num);  // 맵에서 제거(또 입차할 가능성)
-            res.put(num, res.getOrDefault(num, 0) + diff);    // 차별 누적 주차 시간 추가
+        
+        // 입차 이후 출차된 내역이 없으면 23:59에 출차된 것으로 간주
+        for(String car : in.keySet()) {
+            int time = calculateTime("23:59");
+            parking.put(car, parking.getOrDefault(car, 0) + time - in.get(car));
         }
-
-        // 3. 차별 주차요금 계산
-        int[] answer = new int[res.size()];
-        int idx = 0;
-
-        for(String num : res.keySet()) {
-            int time = res.get(num);
-            int money = fees[1];    // 기본 요금
-
-            if(time > fees[0]) {    // 누적 주차 시간이 기본 시간을 초과하면
-                money += (time - fees[0] + fees[2] - 1) / fees[2] * fees[3];
+        
+        // 2. 차량별 누적 시간 기반으로 요금 청구
+        for(String car : parking.keySet()) {
+            int time = parking.get(car);
+            int fee = fees[1];  // 기본 시간 이하라면, 기본 요금 청구(항상 기본 요금을 기본으로 청구함)
+            
+            // 2. 기본 시간 초과 시, 초과한 시간에 대해 단위 시간 마다 단위 요금 청구
+            if(time > fees[0]) {
+                int unit = (time - fees[0]) / fees[2];
+                if((time - fees[0]) % fees[2] != 0) {   // 올림
+                    unit ++;
+                }
+                fee += unit * fees[3];
             }
-
-            answer[idx++] = money;
+            
+            answer.add(fee);    
         }
-
-        return answer;
+        
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+    
+    int calculateTime(String stime) {
+        int hour = Integer.parseInt(stime.substring(0, 2));
+        int min = Integer.parseInt(stime.substring(3, 5));
+        
+        return hour * 60 + min;
     }
 }
