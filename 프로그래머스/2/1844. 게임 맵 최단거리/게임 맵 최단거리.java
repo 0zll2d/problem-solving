9b20a61eb83e684e9be6e127f1f0fa20411cbc42
0zll2d <djadpfa1002@gmail.com>
2026-07-21 12:37:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.23 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,52 @@
 import java.util.*;
 
 class Solution {
+    
+    int n, m;
     int[] dx = {-1, 1, 0, 0};
     int[] dy = {0, 0, -1, 1};
-
-    boolean isOutOfBounds(int n, int m, int x, int y) {
-        return (x < 0 || x >= n || y < 0 || y >= m);
-    }
-
-    // 풀이 1. BFS
+    
     public int solution(int[][] maps) {
-        int answer = -1;
-
-        int n = maps.length;
-        int m = maps[0].length;
+        int answer = 0;
+        
+        n = maps.length;
+        m = maps[0].length;
+        
+        answer = bfs(maps);
+        
+        return answer;
+    }
+    
+    int bfs(int[][] maps) {
+        Deque<int[]> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
-
-        Deque<int[]> q = new ArrayDeque<>();    // { x, y }
-        q.offer(new int[]{0, 0});
+        
+        q.push(new int[]{0, 0, 1});
         visited[0][0] = true;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
             int x = cur[0];
             int y = cur[1];
-
+            int dist = cur[2];
+            
             if(x == n - 1 && y == m - 1) {
-                answer = maps[x][y];
-                break;
+                return dist;
             }
-
+            
             for(int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-
-                if(!isOutOfBounds(n, m, nx, ny) && maps[nx][ny] == 1 && !visited[nx][ny]) {
-                    q.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    maps[nx][ny] = maps[x][y] + 1;
+                
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx][ny] == 0 || visited[nx][ny]) {
+                    continue;
                 }
+                
+                q.offer(new int[]{nx, ny, dist + 1});
+                visited[nx][ny] = true;
             }
         }
-
-        return answer;
+        
+        return -1;
     }
 }
