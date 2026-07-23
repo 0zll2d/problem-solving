8d4c08652de872da499c3129a02de4aa0fc8c599
0zll2d <djadpfa1002@gmail.com>
2026-07-23 18:26:52 +0900

    [level 2] Title: 기능개발, Time: 3.57 ms, Memory: 90.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 74 MB, 시간: 2.75 ms
+메모리: 90.8 MB, 시간: 3.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 23일 18:19:35
+2026년 07월 23일 18:26:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,32 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        
         List<Integer> answer = new ArrayList<>();
+        Deque<Integer> stk = new ArrayDeque<>();
         
-        int N = progresses.length;
-        
-        int[] days = new int[N];
-        
-        for(int i = 0; i < N; i++) {
+        for(int i = 0; i < progresses.length; i++) {
+            // 작업일 계산하기
             int day = (100 - progresses[i]) / speeds[i];
             if((100 - progresses[i]) % speeds[i] != 0) {    // 올림(예: 95%, 개발 속도 4% = 2일)
                 day++;
             }
-            days[i] = day;
-        }
-        
-        Deque<Integer> stk = new ArrayDeque<>();
-        
-        // 예: 7 3 9 
-        for(int day : days) {
-            // 7과 9는 스택에 추가
+            
+            // 작업일 기반으로 배포 개수 구하기
             if(stk.isEmpty() || stk.peek() < day) {
                 stk.push(day);
                 answer.add(1);
-            } else {    // 3은 7에 합쳐 기능 배포
+            } else {
                 int idx = answer.size() - 1;
                 answer.set(idx, answer.get(idx) + 1);
             }
+            
         }
         
         return answer.stream().mapToInt(i -> i).toArray();
