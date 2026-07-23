d69e02ed365cb20ead693d42104b534bf032e697
0zll2d <djadpfa1002@gmail.com>
2026-07-23 20:42:54 +0900

    [level 3] Title: 합승 택시 요금, Time: 21.02 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -4,51 +4,82 @@ class Solution {
     
     int INF = Integer.MAX_VALUE;
     
+    static class Node {
+        int idx;
+        int cost;   // 누적 비용
+        
+        Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+    }
+    
     public int solution(int n, int s, int a, int b, int[][] fares) {
         
         int answer = INF;
         
+        // 1. 그래프 만들기
+        List<Node>[] graph = new ArrayList[n + 1];
         int[][] dist = new int[n + 1][n + 1];
         
         for(int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
             Arrays.fill(dist[i], INF);
-            dist[i][i] = 0;
         }
         
-        for(int[] fare : fares) {
+        for(int[] fare: fares) {
             int c = fare[0];
             int d = fare[1];
             int f = fare[2];
             
-            dist[c][d] = f;
-            // 양방향 연결
-            dist[d][c] = f;
+            // 양방향
+            graph[c].add(new Node(d, f));
+            graph[d].add(new Node(c, f));
         }
         
-        // [플로이드 와샬] x ~ y까지 최단거리 = x ~ m까지 최단거리 + m ~ y까지 최단거리
-        for(int m = 1; m <= n; m++) {
-            for(int x = 1; x <= n; x++) {
-                for(int y = 1; y <= n; y++) {
-                    if(dist[x][m] == INF || dist[m][y] == INF) {
-                        continue;
-                    }
-                    
-                    if(dist[x][y] > dist[x][m] + dist[m][y]) {
-                        dist[x][y] = dist[x][m] + dist[m][y];
-                    }
-                }
-            }
-        }
+        // 2. [다익스트라] 최단 거리 구하기
+        //      (1) S 정점에서 다른 정점 최단 거리 구하기
+        dijkstra(n, graph, dist, s);
+        //      (2) A 정점에서 다른 정점 최단 거리 구하기
+        dijkstra(n, graph, dist, a);
+        //      (3) B 정점에서 다른 정점 최단 거리 구하기
+        dijkstra(n, graph, dist, b);
         
         for(int i = 1; i <= n; i++) {
-            if(dist[s][i] != INF && dist[i][a] != INF && dist[i][b] != INF) {
-                // A까지 택시 요금 + B까지 택시 요금
-                int cost = dist[s][i] + dist[i][a] + dist[i][b];
-                answer = Math.min(answer, cost);
+            if(dist[s][i] == INF || dist[a][i] == INF || dist[b][i] == INF) {
+                continue;
             }
+            
+            answer = Math.min(answer, dist[s][i] + dist[a][i] + dist[b][i]);
         }
-        
+    
         return answer;
     }
     
+    void dijkstra(int n, List<Node>[] graph, int[][] dist, int idx) {
+        PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> (a.cost - b.cost));
+        
+        pq.offer(new Node(idx, 0));
+        dist[idx][idx] = 0;
+        
+        while(!pq.isEmpty()) {
+            Node cur = pq.poll();
+            int now = cur.idx;
+            int cost = cur.cost;
+            
+            if(dist[idx][now] < cost) {
+                continue;
+            }
+            
+            for(Node edge : graph[now]) {
+                int next = edge.idx;
+                int newCost = dist[idx][now] + edge.cost;
+                
+                if(dist[idx][next] > newCost) {
+                    pq.offer(new Node(next, newCost));
+                    dist[idx][next] = newCost;
+                }
+            }
+        }
+    }
 }
