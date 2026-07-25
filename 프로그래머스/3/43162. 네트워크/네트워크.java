cdabc0d244ce4e52e735c5e115a98f66bef3d936
0zll2d <djadpfa1002@gmail.com>
2026-07-25 18:25:16 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,30 +1,48 @@
+// 유니온 파인드 - 같은 집합인지 찾기
+import java.util.*;
+
 class Solution {
     
-    boolean[] visited;
+    int[] parent;
+    int answer; // 그룹의 개수
     
     public int solution(int n, int[][] computers) {
+        answer = n;
         
-        visited = new boolean[n];
+        parent = new int[n]; // 자신(idx)의 부모를 담는 배열 설정
         
-        int answer = 0; // 연결된 네트워크 수
-                
         for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                dfs(n, computers, i);
-                answer++;
+            parent[i] = i;  // 현재 부모는 자기 자긴(idx)
+        }
+        
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(computers[i][j] == 1) {
+                    union(i, j);
+                }
             }
         }
         
         return answer;
     }
     
-    void dfs(int n, int[][] computers, int idx) {
-        visited[idx] = true;
+    void union(int a, int b) {
+        int ra = find(a);
+        int rb = find(b);
         
-        for(int i = 0; i < n; i++) {
-            if(!visited[i] && computers[idx][i] == 1) {
-                dfs(n, computers, i);
-            }
+        if(ra == rb) {  // 사이클 검사, 이미 같은 그룹인데 또 그룹으로 넣으려고 할 때
+            return;
         }
+        
+        parent[rb] = ra;
+        answer--;   // 그룹으로 합쳐지면 -1
+    }
+    
+    int find(int x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        
+        return parent[x];
     }
 }
